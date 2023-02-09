97d2ddaad00ef25b9a8299ca39b541d270809a2e
SwallowedStar <etienne.malacarne@gmail.com>
2023-02-09 13:41:18 +0100

    test

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(logger('dev'));
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
 
 // Setup HTTP server
 const server = http.createServer(app);
--- routes/views.js
+++ routes/views.js
@@ -1,10 +1,16 @@
 const express = require("express")
 
+
 const viewRouter = express.Router()
 
 // Connection screen
 viewRouter.get("/", async function(req, res){
-    res.render()
+    res.render("home")
 })
+
+viewRouter.get("/connexion", async function(req, res){
+    res.render("connexion")
+})
+
 
 module.exports = viewRouter
